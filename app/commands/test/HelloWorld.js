fb3b962db3155d70b041744cee8997b1a60ed2a2
macen <ausmosismason@gmail.com>
2021-05-12 02:01:44 -0400

    Fixed spelling

--- app/commands/test/HelloWorld.js
+++ app/commands/test/HelloWorld.js
@@ -2,7 +2,6 @@
 
 //Exports the command as a function to be read by the load-Commands.js file on bot start up.
 module.exports = {
-    
     //CommandOptions
     name: 'HelloWorld', // Command name
     commands: ['helloWorld', 'hw', 'test'], // Command aliases.
@@ -18,27 +17,30 @@ module.exports = {
     //Name: '', Commands: [''] are required.
     
     callback: (message, arguments, text, client) => {
-    //' callback: () =>{}' Excutes the custom command code.
+    //' callback: () =>{}' Executes the custom command code.
     // '(message, arguments, text, client)' Passes through 'message', 'arguments', 'text', 'client'.
     // Must be in that order to properly pass through.
     
     // Text is all arguments joined.
-    // Used to input more then 1 word in an argument.
+    // Used to input more than 1 word in an argument.
     // Set maxArgs to 100 to use text.
     // You can also recreate this with just 
-    //```
+    
+    //```js
+
     //arguments.join(" ")
+
     //```
         const input = arguments[0]
-    // Asign the first argument to the const 'input'.
+    // Assign the first argument to the const 'input'.
 
         if(!input){
-    // Check to see if and argument was provided.
-    // If not then excute 'message.channel.send('Hello!')'.
+    // Check to see if an argument was provided.
+    // If not then execute 'message.channel.send('Hello!')'.
           return message.channel.send('Hello!')
     // Sends 'hello' back to the channel that the command was executed in.
     // Then exits the command with return 
-    // Stoping the rest of the code from excuting when we don't want it to.
+    // Stopping the rest of the code from executing when we don't want it to.
         }
     // Else if an argument was provided.
         message.channel.send(input)
